07fc39aafcddf88b74e406dbc495c4220fd1111b
michael haruo nakashima <mhnakashima@gmail.com>
2021-08-20 12:17:24 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { teste } from './components/teste';
+import './styles/style.scss';
+
+console.log('hello, world');
+
+const testMessage: string = 'TypeScript works';
+teste();
+console.log(testMessage);
